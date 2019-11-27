8e924c653011dcd205f91d6a9476a3f7450c9ae4
Reid Chambers <reid.chambers@cooper.edu>
2019-11-27 16:54:42 -0500

    Completed assignment with descriptive text and added sample input data file

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import sys
-
+import csv
+from statistics import mean, median
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
 
-    input_file_path = sys.argv[1]
-    print(f"Processing input file: {input_file_path}")
+    f = input_file = sys.argv[1]
+    print(f"Processing input file: {input_file}")
 
     # TODO: Fill in the actual logic here!
+    with open(input_file) as f:
+        readcsv = csv.reader(f, delimiter = ',')
+
+        for line in readcsv:
+            lst4avg = []
+            print(line)
+            for item in line:
+                lst4avg.append(float(item))
+            print(f"The mean of this list is: {mean(lst4avg)}\n")
 
 
 if __name__ == "__main__":
